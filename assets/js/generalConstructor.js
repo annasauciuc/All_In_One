b66dba06987cbdf8794e905090c1fd838441da67
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-22 11:12:06 +0000

    adding the general constryctor

---
+++ assets/js/generalConstructor.js
@@ -0,0 +1,56 @@
+/**
+ * This class manage all the complex HTML elements inside the
+ * center container, it allow us to manage our
+ * logic more easily and empty & append new HTML elements smoothly.
+ * @return {function} getInstance
+ */
+const GeneralConstructor = (function() {
+    let instance;
+    let constructors;
+  
+    const targetContainer = $("#mainContainer");
+  
+    function init(classes) {
+      constructors = classes;
+  
+      function construct(constructorKey) {
+        if (
+          constructors[constructorKey] &&
+          targetContainer.find(constructors[constructorKey]["container"])
+            .length === 0
+        ) {
+          constructors[constructorKey]["constructor"].construct(targetContainer);
+        }
+      }
+  
+      return {
+        constructors,
+        construct
+      };
+    }
+  
+    return {
+      getInstance: function(classes) {
+        if (!instance && classes instanceof Object) {
+          instance = init(classes);
+        } else {
+          throw new Error(
+            "GeneralConstructor receive a bad parameter: " + classes
+          );
+        }
+        return instance;
+      }
+    };
+  })();
+  
+  let generalConstructor;
+  
+  try {
+    generalConstructor = GeneralConstructor.getInstance({
+      "users-table": { container: "#mainPage", constructor: mainPage },
+      "user-form": { container: "#user-form-container", constructor: userForm },
+      
+    });
+  } catch (err) {
+    alert(err.name + " " + err.message);
+  }
